7825beed8637238724cdbc257f9dfbc6201c8a64
Ryan Lirio <ryanlirio2@gmail.com>
2024-11-11 09:32:34 -0300

    first commit

--- JavaScript_II/JS_IIexercício3/objeto.js
+++ JavaScript_II/JS_IIexercício3/objeto.js
@@ -1,33 +1,84 @@
-let imoveisCadastrados = [[]];
+let imoveisCadastrados = [];
 let menu = true;
-
+let i = 0;
+let j = 0;
+let sairdoloop = true;
 while(menu)
 {
     let acao = prompt("Quantidade de imóveis cadastrados: " + imoveisCadastrados.length + "\n"
         + "O que deseja fazer?\n1 - Salvar um imóvel\n2 - Mostrar os imóveis salvos\n0 - Sair");
 
+    sairdoloop = true;
     switch(acao)
     {
         case "1":
-            for(let i = 0; i < 9; i++)
-            {
+            while(sairdoloop){
+            let nomeProprietario = prompt("Qual o nome do proprietário?");
+            let quartos = prompt("Qual a quantidade de quartos desse imóvel?");
+            let banheiros = prompt("Qual a quantidade de banheiros?");
+            let garagem = prompt ("Possui garagem?");
+            sairdoloop = prompt("Gostaria de adicionar mais algum imóvel?");
 
-                imoveisCadastrados[i][0] = prompt("Qual o nome do proprietário?");
-                imoveisCadastrados[i][1] = prompt("Qual a quantidade de quartos desse imóvel?");
-                imoveisCadastrados[i][2] = prompt("Qual a quantidade de banheiros?");
-                imoveisCadastrados[i][3] = prompt ("Possui garagem?");
-                let sairdoloop = prompt("Gostaria de adicionar mais algum imóvel?")
-                if(sairdoloop == "sim")
+            let imovel = {
+                proprietário: nomeProprietario,
+                quartos: quartos,
+                banheiros: banheiros,
+                garagem: garagem
+            }
+            imoveisCadastrados.push(imovel);
+            console.log("Imovel cadastrado com sucesso!");
+            if(sairdoloop == "não"){
+                sairdoloop = false;
+            }
+            }
+
+            break;
+        case "2":
+            if(imoveisCadastrados.length === 0)
+            {
+                console.log("Não existe nenhum imóvel cadastrado")
+            }else       
+            {
+            imoveisCadastrados.forEach(function(item, index){
+                console.log("Imóvel " + (index+1));
+                console.log("Vendido por: " + item.proprietário);
+                console.log("Quartos: " + item.quartos);
+                console.log("Garagem: " + item.garagem);
+                console.log("");
+                
+            });
+            }
+            console.log(imoveisCadastrados);
+            break;
+        case "0":
+            console.log("Saindo...");
+            menu = false;
+        default:
+            console.log("Opção inválida!")
+            break;
+    }
+}
+
+/*for(; i < 9; i++)
+            {
+                imoveisCadastrados[i] = "imovel " + (i+1) + [[]];
+                
+                for(; i < 9; j++)
+                {
+                    
+                    imoveisCadastrados[i][j] = ["Nome do proprietário: " + nomeProprietario,
+                        "Quartos: " + quartos,
+                        "Banheiros: " + banheiros,
+                        "Garagem: " + garagem
+                    ]
+                    
+                    if(sairdoloop == "não")
                     {
                         i = 9;
                     }
-            }
-            console.log(imoveisCadastrados[0]);
-            
-            break;
-        case "2":
-        case "0":
-            menu = false;
-        default:
-    }
-}
+                    
+                }
+                //imoveisCadastrados.push(nomeProprietario, quartos, banheiros, garagem);
+                console.log(imoveisCadastrados);
+                console.log(imoveisCadastrados[0][0])
+            }*/
